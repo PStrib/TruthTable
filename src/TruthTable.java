cd63746ba63744485ef9d1fb32ebf417cf23cabe
Peter Stribblehill <peter.strib@gmail.com>
2022-10-19 18:42:39 +0100

    Creates a truth-table of n variables but breaks if you go over 10

--- src/TruthTable.java
+++ src/TruthTable.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 
+import static java.lang.String.format;
+
 public class TruthTable {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
+
         System.out.println("Enter how many variables you need");
         int variables=scan.nextInt();
 
-        for (int i=0; i<=variables; i++){
-            System.out.println(Integer.toBinaryString(i));
+        int rows=(1<<(variables));
+        for (int i=0; i<=rows-1; i++){
+            System.out.format("%0"+variables+"d", Integer.valueOf(Integer.toBinaryString(i)));
+            System.out.print("|\n");
         }
+
     }
 }
